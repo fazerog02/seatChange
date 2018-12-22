c014b0cffc1b0966676d2087153f686c1d9a9639
fazerog02 <39889014+fazerog02@users.noreply.github.com>
2018-12-22 11:51:38 +0900

    いろいろメッセージを追加した

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main(){
-    cout << "INPUT : HOW MANY PEOPLE" << endl;
+    cout << "人数を入力" << endl;
     int peopleNum;
     cin >> peopleNum;
     vector<int> seatNum(peopleNum);
     for(int i = 0; i < peopleNum; i++){
-        seatNum[i] = 10000;
+        seatNum[i] = -1;
     }
-    //setrandom
+    //setRandom
     random_device rnd;
     int rndNum;
     for(int i = 0; i < peopleNum; i++){
@@ -24,10 +24,24 @@ int main(){
     //config
     string pos;
     int pos_stoi, val;
-    int counter;
+    int counter = 0;
+    cout << "固定席の設定（endで終了）" << endl;
+    cout << "1~人数までの数字で席を指定する" << endl;
+    cout << "指定する際の席の番号" << endl;
+    for(int i = 0; i < peopleNum; i++){
+        cout << '[';
+        if(seatNum[i] / 10 == 0) cout << 0;
+        cout << i+1 << ']';
+        if(counter == 5){
+            cout << endl;
+            counter = -1;
+        }
+        counter++;
+    }
+    cout << endl;
     while(1){
-        cout << "INPUT : ABSORUTE SEAT(input 'end' END)" << endl;
         counter = 0;
+        cout << "席を指定" << endl;
         cin >> pos;
         if(pos == "end"){
             for(int i = 0; i < 20; i++){
@@ -35,12 +49,14 @@ int main(){
             }
             break;
         }
+        cout << "出席番号を入力" << endl;
         cin >> val;
         pos_stoi = stoi(pos);
         seatNum[pos_stoi-1] = val;
+        cout << "プレビュー" << endl;
         for(int i = 0; i < peopleNum; i++){
             cout << '[';
-            if(seatNum[i] / 10 == 0) cout << 0;
+            if(seatNum[i] / 10 == 0 && seatNum[i] != -1) cout << 0;
             cout << seatNum[i] << ']';
             if(counter == 5){
                 cout << endl;
