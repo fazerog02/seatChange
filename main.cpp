7b64f02d548666afa34f4125acda4f8fd49b36eb
fazerog02 <fazerog02@gmail.com>
2018-12-22 12:12:37 +0900

    [add]不正な入力対策

--- main.cpp
+++ main.cpp
@@ -2,33 +2,53 @@
 #include <unistd.h>
 using namespace std;
 
+bool isnum;
+bool isNum(string s){
+    isnum = true;
+    for(int i = 0; i < s.size(); i++){
+        if(!isdigit(s[i])) isnum = false;
+    }
+    if(isnum){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int main(){
+reinput_peopleNum:
     cout << "人数を入力" << endl;
-    int peopleNum;
+    string peopleNum;
+    int peopleNum_stoi;
     cin >> peopleNum;
-    vector<int> seatNum(peopleNum);
-    for(int i = 0; i < peopleNum; i++){
+    if(!isNum(peopleNum)){
+        cout << "無効な入力" << endl;
+        goto reinput_peopleNum;
+    }
+    peopleNum_stoi = stoi(peopleNum);
+    vector<int> seatNum(peopleNum_stoi);
+    for(int i = 0; i < peopleNum_stoi; i++){
         seatNum[i] = -1;
     }
     //setRandom
     random_device rnd;
     int rndNum;
-    for(int i = 0; i < peopleNum; i++){
+    for(int i = 0; i < peopleNum_stoi; i++){
         retry:
-        rndNum = rnd() % peopleNum + 1;
-        for(int j = 0; j < peopleNum; j++){
+        rndNum = rnd() % peopleNum_stoi + 1;
+        for(int j = 0; j < peopleNum_stoi; j++){
             if(seatNum[j] == rndNum) goto retry;
         }
         seatNum[i] = rndNum;
     }
     //config
-    string pos;
-    int pos_stoi, val;
+    string pos, val;
+    int pos_stoi, val_stoi;
     int counter = 0;
     cout << "固定席の設定（endで終了）" << endl;
     cout << "1~人数までの数字で席を指定する" << endl;
     cout << "指定する際の席の番号" << endl;
-    for(int i = 0; i < peopleNum; i++){
+    for(int i = 0; i < peopleNum_stoi; i++){
         cout << '[';
         if((i+1) / 10 == 0) cout << ' ';
         cout << i+1 << ']';
@@ -41,6 +61,7 @@ int main(){
     cout << endl;
     while(1){
         counter = 0;
+reinput_pos:
         cout << "席を指定" << endl;
         cin >> pos;
         if(pos == "end"){
@@ -49,12 +70,30 @@ int main(){
             }
             break;
         }
+        if(!isNum(pos)){
+            cout << "無効な入力" << endl;
+            goto reinput_pos;
+        }
+        pos_stoi = stoi(pos);
+        if(pos_stoi > peopleNum_stoi){
+            cout << "無効な入力" << endl;
+            goto reinput_pos;
+        }
+reinput_val:
         cout << "出席番号を入力" << endl;
         cin >> val;
-        pos_stoi = stoi(pos);
-        seatNum[pos_stoi-1] = val;
+        if(!isNum(val)){
+            cout << "無効な入力" << endl;
+            goto reinput_val;
+        }
+        val_stoi = stoi(val);
+        if(val_stoi > peopleNum_stoi){
+            cout << "無効な入力" << endl;
+            goto reinput_val;
+        }
+        seatNum[pos_stoi-1] = val_stoi;
         cout << "プレビュー" << endl;
-        for(int i = 0; i < peopleNum; i++){
+        for(int i = 0; i < peopleNum_stoi; i++){
             cout << '[';
             if(seatNum[i] / 10 == 0 && seatNum[i] != -1) cout << 0;
             cout << seatNum[i] << ']';
@@ -78,7 +117,7 @@ int main(){
     cout << "GO!!!" << endl << endl;
     sleep(1);
     cout << "-------黒板--------" << endl << endl;
-    for(int i = 0; i < peopleNum; i++){
+    for(int i = 0; i < peopleNum_stoi; i++){
         cout << '[';
         if(seatNum[i] / 10 == 0) cout << 0;
         cout << seatNum[i] << ']';
@@ -90,6 +129,11 @@ int main(){
         counter++;
     }
     cout << endl << endl;
-    cout << "FINISH!!!!" << endl;
-    while(getchar() != ' ');
+    cout << "FINISH!!!!" << endl << endl;
+    cout << "endで終了" << endl;
+    string end;
+    while(1){
+        cin >> end;
+        if(end == "end") break;
+    }
 }
