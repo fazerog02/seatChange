fe93f5e9c219c8355ae8d79790a3d66a967c49f8
fazerog02 <fazerog02@gmail.com>
2019-06-17 14:01:41 +0900

    [fix]りふぁくたりんぐ

--- seatChanger.cpp
+++ seatChanger.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 
-
-//文字列が数字かどうか判定する
+// 文字列が数字かどうか判定する
 bool IsNumber(string s){
     bool is_number = true;
     for(int i = 0; i < s.size(); i++){
@@ -14,172 +13,130 @@ bool IsNumber(string s){
 }
 
 
-
 int main(){
-
-
-    //人数の処理
-    string number_of_people_string;
-    int number_of_people_int;
-reinput_number_of_people_string:
-    cout << "人数を入力してください" << endl;
-    cout << ">>> ";
-    cin >> number_of_people_string; //stringで受けておく
-    if(!IsNumber(number_of_people_string)){
-        //数字以外の場合は再入力させる
-        cout << "ERROR: 数字を入力してください" << endl;
-        goto reinput_number_of_people_string;
+    string num_of_people_str = "default";
+    int num_of_people;
+    while(!IsNumber(num_of_people_str)){
+        cout << "人数を入力してください" << endl;
+        cout << ">>> ";
+        cin >> num_of_people_str;  // 文字を入力されたときのためにstringで受けておく
     }
-    number_of_people_int = stoi(number_of_people_string); //stringで受けていたものをintに戻す
+    num_of_people = stoi(num_of_people_str);  // stringで受けていたものをintに戻す
 
-
-    //座席表の出席番号を-1で初期化しておく
-    vector<int> field_of_seat(number_of_people_int);
-    for(int i = 0; i < number_of_people_int; i++){
-        field_of_seat[i] = -1;
+    // 座席表の出席番号を-1で初期化しておく
+    vector<int> seating_chart(num_of_people);
+    for(int i = 0; i < num_of_people; i++){
+        seating_chart[i] = -1;
     }
 
-
-    //設定
-    string selected_position_string, selected_seat_number_string;
-    int selected_position_int, selected_seat_number_int;
-    vector<int> selected_position_list, selected_seat_number_list;
-    cout << "限定ランダムの設定(目が悪い人など)[endで終了]" << endl;
+    // 限定ランダムの設定
+    cout << "限定ランダムの設定(目が悪い人など用)" << endl;
+    // 席の番号表を表示する
     cout << "席の番号" << endl;
-
-
-    //席の番号表を表示する
-    for(int i = 1; i <= number_of_people_int; i++){
+    for(int i = 1; i <= num_of_people; i++){
         printf("%c%2d%c ", '[', i, ']');
-        if(i % 6 == 0) cout << endl; //6個ずつで改行する
+        if(i % 6 == 0) cout << endl;  // 6個ずつで改行する
     }
     cout << endl;
 
-
-    //限定でランダムに選ぶ席を指定する
-    cout << "限定でランダムに選ぶ席を指定してください" << endl;
+    // 限定でランダムに選ぶ席を指定する
+    string selected_position_str = "default";
+    int selected_position;
+    vector<int> selected_position_list;
+    cout << "限定でランダムに選ぶ席を指定してください[endで終了]" << endl;
     while(1){
-    reinput_selected_position_string:
+        cout << "席番号を入力してください" << endl;
         cout << ">>> ";
-        cin >> selected_position_string; //stringで受けておく
+        cin >> selected_position_str;  // 文字を入力されたときのためにstringで受けておく
+        if(selected_position_str == "end") break;
+        if(!IsNumber(selected_position_str)) continue;
 
-        if(selected_position_string == "end"){
-            //endと入力されたら画面をきれいにして終了する
-            system("clear");
-            break;
-        }
-        if(!IsNumber(selected_position_string)){
-            //数字以外の場合は再入力させる
-            cout << "数字を入力してください" << endl;
-            goto reinput_selected_position_string;
-        }
-        selected_position_int = stoi(selected_position_string); //stringで受けていたものをintに戻す
-
-        if(selected_position_int > number_of_people_int){
-            //入力された座席の番号が存在しない場合は再入力させる
-            cout << "ERROR: その座席は存在しません" << endl;
-            goto reinput_selected_position_string;
-        }
-
-        selected_position_list.push_back(selected_position_int); //問題がないのでリストに座席の番号を追加する
+        selected_position = stoi(selected_position_str);  // stringで受けていたものをintに戻す
+        selected_position_list.push_back(selected_position);  // リストに座席の番号を追加する
     }
+    system("clear");
 
-
-    //限定でランダムに選ぶ出席番号を指定する
-    cout << "限定でランダムに選ぶ出席番号を指定してください" << endl;
+    // 限定でランダムに選ぶ出席番号を指定する
+    string selected_seat_num_str = "default";
+    int selected_seat_num;
+    vector<int> selected_seat_num_list;
+    cout << "限定でランダムに選ぶ出席番号を指定してください[endで終了]" << endl;
     while(1){
-    reinput_selected_seat_number:
+        cout << "出席番号を入力してください" << endl;
         cout << ">>> ";
-        cin >> selected_seat_number_string;
+        cin >> selected_seat_num_str;  // 文字を入力されたときのためにstringで受けておく
+        if(selected_seat_num_str == "end") break;
+        if(!IsNumber(selected_seat_num_str)) continue;
 
-        if(selected_seat_number_string == "end"){
-            //endと入力されたら画面をきれいにして終了する
-            system("clear");
-            break;
-        }
-        if(!IsNumber(selected_seat_number_string)){
-            //数字以外の場合は再入力させる
-            cout << "数字を入力してください" << endl;
-            goto reinput_selected_seat_number;
-        }
-
-        selected_seat_number_int = stoi(selected_seat_number_string);//stringで受けていたものをintに戻す
-
-        if(selected_seat_number_int > number_of_people_int){
-            //入力された出席番号が存在しない場合は再入力させる
-            cout << "ERROR: その出席番号は存在しません" << endl;
-            goto reinput_selected_seat_number;
-        }
-
-        selected_seat_number_list.push_back(selected_seat_number_int); //問題がないのでリストに出席番号を追加する
+        selected_seat_num = stoi(selected_seat_num_str);  // stringで受けていたものをintに戻す
+        selected_seat_num_list.push_back(selected_seat_num);  // リストに座席の番号を追加する
     }
+    system("clear");
 
-
-    //限定ランダムで先に座席を決める
+    // 限定ランダムの座席を先に決める
     random_device random;
     int random_number;
+    bool does_exist;
+    int random_seat_position;
+    // 限定ランダムの各出席番号に対応する座席を決める
+    for(int i = 0; i < selected_seat_num_list.size(); i++){
+        does_exist = true;
+        while(does_exist){
+            does_exist = false;
+            // 指定された座席からランダムで選ぶ
+            random_number = random() % selected_position_list.size();
+            random_seat_position = selected_position_list[random_number]-1;
 
-    bool is_exist;
-
-    for(int i = 0; i < selected_seat_number_list.size(); i++){
-    retry_closed_random:
-        is_exist = false;
-        random_number = random() % selected_position_list.size(); //指定された座席からランダムで選ぶ
-        if(field_of_seat[selected_position_list[random_number]-1] != -1) is_exist = true; //すでに選ばれているかどうか確認する
-        if(is_exist){
-            //すでに選ばれていたらもう一度指定された座席からランダムで選び直す
-            goto retry_closed_random;
-        } else {
-            field_of_seat[selected_position_list[random_number]-1] = selected_seat_number_list[i]; //座席を決める
+            if(seating_chart[random_seat_position] != -1) does_exist = true;  // すでに選ばれているかどうか確認する
         }
+        seating_chart[random_seat_position] = selected_seat_num_list[i];  // 座席表に限定ランダムの結果を代入する
     }
 
-
-    //座席を決める
-    for(int i = 0; i < number_of_people_int; i++){
-        if(field_of_seat[i] != -1) continue; //すでに決めてあったらスキップする
-    retry_random:
-        random_number = (random() % number_of_people_int) + 1; //ランダムで出席番号を一つ選ぶ
-        //すでに出席番号が使われていたらもう一度ランダムで出席番号を一つ選び直す
-        for(int j = 0; j < number_of_people_int; j++){
-            if(field_of_seat[j] == random_number) goto retry_random;
+    // 座席をランダムに決める
+    for(int i = 0; i < num_of_people; i++){
+        if(seating_chart[i] != -1) continue;  // すでに決めてあったらスキップする
+        does_exist = true;
+        while(does_exist){
+            does_exist = false;
+            random_number = (random() % num_of_people) + 1;  // ランダムで出席番号を一つ選ぶ
+            // 選んだ出席番号がすでに使われているかどうか確かめる
+            for(int j = 0; j < num_of_people; j++){
+                if(seating_chart[j] == random_number) does_exist = true;
+            }
         }
-
-        field_of_seat[i] = random_number; //座席を決める
+        seating_chart[i] = random_number;  // 座席表にランダムの結果を代入する
     }
 
-
-    //座席表を表示する
+    // 座席表を表示する
     cout << "Enterを押してスタート" << endl;
-    //Enterの入力待ち
+    // Enterの入力待ち
     while(getchar() != '\n');
     while(getchar() != '\n');
 
-    //３秒カウントする
+    // ３秒カウントする
     for(int i = 3; i >= 1; i--){
         cout << i << endl;
-        if(number_of_people_int <= 100)sleep(1);
+        sleep(1);
     }
     cout << "GO!" << endl << endl;
     sleep(1);
 
+    // 表を表示する
     cout << "-------黒板--------" << endl << endl;
-    //表を表示する
-    for(int i = 0; i < number_of_people_int; i++){
-        printf("%c%2d%c ", '[', field_of_seat[i], ']');
-        if((i+1) % 6 == 0) cout << endl; //6個ずつで改行する
+    for(int i = 0; i < num_of_people; i++){
+        printf("%c%2d%c ", '[', seating_chart[i], ']');
+        if((i+1) % 6 == 0) cout << endl;  // 6個ずつで改行する
     }
     cout << endl << endl;
-    cout << "FINISH!" << endl << endl;
 
+    cout << "FINISH!" << endl << endl;
     cout << "endで終了" << endl;
     string end;
     while(1){
-        //endの入力待ち
+        // endの入力待ち
         cin >> end;
         if(end == "end") break;
     }
-
 
     return 0;
 }
